c2db35e9ccadfbcb805e9d3e1eed1dbe47acfc52
JinWoo_ <829kong@naver.com>
2022-09-27 10:52:52 +0900

    2022-09-27 초기설정 ( Texture Mapping)

---
+++ Engine/RootSignature.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// [계약서 / 결재]
+
+// CPU [        ]    GPU [        ]
+// 한국 [        ]   베트남 [       ]
+
+class RootSignature
+{
+public:
+	void Init();
+
+	ComPtr<ID3D12RootSignature>	GetSignature() { return _signature; }
+
+private:
+	void CreateSampleDesc();
+	void CreateRootSignature();
+
+private:
+	ComPtr<ID3D12RootSignature>	_signature;
+	D3D12_STATIC_SAMPLER_DESC _sampleDesc;
+};
+
+
+
